80f3706b3e7fdb2fd42997551a6186bd6a0a990a
Benny Dabush <bennydabush@gmail.com>
2024-03-27 18:32:49 +0200

    hw - nestend navigation & use nav bar link

--- cards-d100923er/src/layout/header/topNavBar/logo/Logo.jsx
+++ cards-d100923er/src/layout/header/topNavBar/logo/Logo.jsx
@@ -1,19 +1,23 @@
 import React from "react";
 import { Typography } from "@mui/material";
+import NavBarLink from "../../../../routes/components/NavBarLink";
+import ROUTES from "../../../../routes/routesModel";
 
 export default function Logo() {
   return (
     <>
-      <Typography
-        variant="h4"
-        sx={{
-          marginRight: 2,
-          fontFamily: "fantasy",
-          display: { xs: "none", md: "inline-flex" },
-        }}
-      >
-        BCard
-      </Typography>
+      <NavBarLink to={ROUTES.ROOT}>
+        <Typography
+          variant="h4"
+          sx={{
+            marginRight: 2,
+            fontFamily: "fantasy",
+            display: { xs: "none", md: "inline-flex" },
+          }}
+        >
+          BCard
+        </Typography>
+      </NavBarLink>
     </>
   );
 }
