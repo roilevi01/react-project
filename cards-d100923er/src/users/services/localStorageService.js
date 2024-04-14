f20baf060fd7cf04e033cd4cd74bc2aee543b0c3
Benny Dabush <bennydabush@gmail.com>
2024-04-14 21:16:45 +0300

    local storage service and decode the token

---
+++ cards-d100923er/src/users/services/localStorageService.js
@@ -0,0 +1,20 @@
+import JwtDecode from "jwt-decode";
+
+const TOKEN = "my token";
+
+export const setTokenInLocalStorage = (encryptedToken) => {
+  localStorage.setItem(TOKEN, encryptedToken);
+};
+
+export const removeToken = () => localStorage.removeItem(TOKEN);
+
+export const getToken = () => localStorage.getItem(TOKEN);
+
+export const getUser = () => {
+  try {
+    const myToken = getToken();
+    return JwtDecode(myToken);
+  } catch (error) {
+    return null;
+  }
+};
